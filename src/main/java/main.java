828b665e391ffa65fab0c5348350f1d2a7b8ffe9
Andrey Sender <ansen80@inbox.ru>
2024-08-23 14:27:50 +0300

    Чтение длинны вводимого слова

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Случайное числа от 0 до 1: " + Math.random());
-
+        System.out.print("Введите текст и нажмите <Enter>: ");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
+
