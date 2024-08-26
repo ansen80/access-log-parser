0d42b0343f68039007b2a67a66ea3a7504c3773e
Andrey Sender <ansen80@inbox.ru>
2024-08-26 21:09:33 +0300

    Рассчет чисел по задаче. Читаемость кода

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Введите первое число:");
         int firstNumber = scanner.nextInt();
+
         System.out.println("Введите второе число:");
         int secondNumber = scanner.nextInt();
+
         int sum = firstNumber + secondNumber;
         System.out.println("Сумма: " + sum);
+
         int difference = firstNumber - secondNumber;
         System.out.println("Разность: " + difference);
+
         int product = firstNumber * secondNumber;
         System.out.println("Произведение: " + product);
+
         double quotient = (double) firstNumber / secondNumber;
         System.out.println("Частное: " + quotient);
+
         scanner.close();
     }
 }
