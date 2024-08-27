69e5a3d38dbe8f6736a212b5902882e139888a57
Andrey Sender <ansen80@inbox.ru>
2024-08-27 11:44:16 +0300

    Чтение количества символов в тексте

---
+++ src/main/java/text_length.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class text_length {
+    public static void main(String[] args) {
+        System.out.print("Введите текст и нажмите <Enter>: ");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
+
+    }
+}
