80672c11f92df4de20ecb8843fe54b756c778439
Andrey Sender <ansen80@inbox.ru>
2024-09-04 18:57:25 +0300

    Запрашиваем путь к файлу, который будем парсить

---
+++ src/main/java/Courses/parserLogFile.java
@@ -0,0 +1,29 @@
+package Courses;
+import java.io.File;
+import java.util.Scanner;
+
+public class parserLogFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int validFileCount = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Указанный файл не существует или путь ведет к папке. Попробуйте еще раз.");
+                continue;
+            }
+
+            validFileCount++;
+            System.out.println("Путь указан верно. Это файл номер " + validFileCount);
+        }
+    }
+}
+
