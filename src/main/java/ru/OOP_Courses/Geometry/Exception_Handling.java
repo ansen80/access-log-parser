2e188e7bee3e241544cd30610f386035b0314820
Andrey Sender <ansen80@inbox.ru>
2024-10-18 15:09:58 +0300

    Закоммитили код с длинной min и max. Добавили код для чтения доли запросов от Yandex и Google Ботов!

--- src/main/java/ru/OOP_Courses/Geometry/Exception_Handling.java
+++ src/main/java/ru/OOP_Courses/Geometry/Exception_Handling.java
@@ -5,37 +5,48 @@ public class Exception_Handling {
     public static void main(String[] args) {
         String path = "C:/_git/AccessLogParser/access.log";
         int totalLines = 0;
-        int maxLength = 0;
-        int minLength = Integer.MAX_VALUE;
+        int googlebotRequests = 0;
+        int yandexBotRequests = 0;
 
         try {
             File file = new File(path);
             if (!file.exists() || !file.isFile()) {
-                throw new FileNotFoundException("Указанный файл не существует или это не файл.");
+                throw new FileNotFoundException("Указанный файл не существует или это не файл");
             }
             FileReader fileReader = new FileReader(file);
             BufferedReader reader = new BufferedReader(fileReader);
             String line;
             while ((line = reader.readLine()) != null) {
-                int length = line.length();
                 totalLines++;
-                if (length > maxLength) {
+                /*if (length > maxLength) {
                     maxLength = length;
                 }
                 if (length < minLength) {
                     minLength = length;
-                }
-                if (length > 1024) {
+                }*/
+                if (line.length() > 1024) {
                     reader.close();
                     throw new LineTooLongException("В файле найдена строка длиннее 1024 символов.");
+                }
+                String[] parts = line.split("\"");
+                if (parts.length >= 6) {
+                    String userAgent = parts[parts.length - 1].trim();
+                    if (userAgent.contains("Googlebot")) {
+                        googlebotRequests++;
+                    } else if (userAgent.contains("YandexBot")) {
+                        yandexBotRequests++;
+                    }
                 }
             }
             reader.close();
             System.out.println("Общее количество строк в файле: " + totalLines);
-            System.out.println("Длина самой длинной строки в файле: " + maxLength);
+            /*System.out.println("Длина самой длинной строки в файле: " + maxLength);
             System.out.println("Длина самой короткой строки в файле: " + minLength);
-        } catch (LineTooLongException e) {
-            System.err.println(e.getMessage());
+            */
+            System.out.println("Запросы от Googlebot: " + googlebotRequests);
+            System.out.println("Запросы от YandexBot: " + yandexBotRequests);
+            System.out.println("Доля запросов от Googlebot: " + (googlebotRequests * 100.0 / totalLines) + "%");
+            System.out.println("Доля запросов от YandexBot: " + (yandexBotRequests * 100.0 / totalLines) + "%");
         } catch (IOException e) {
             e.printStackTrace();
         }
